df415a77492ef65e94bf380cb39d6f33a44dcfaa
driti29 <130924378+driti29@users.noreply.github.com>
2023-07-01 17:58:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Rahul Cutie
+          Hi! This is Driti
         </p>
         <a
           className="App-link"
